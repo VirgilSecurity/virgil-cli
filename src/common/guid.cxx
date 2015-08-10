edcec20428e45307ff44b160a95c79eb32dc6fee
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2015-08-10 14:43:27 +0300

    Make ad hoc GUID implementation.

--- src/common/guid.cxx
+++ src/common/guid.cxx
@@ -34,34 +34,28 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
- #include <cli/guid.h>
+#include <cli/guid.h>
 
-extern "C"
-{
-#ifdef WIN32
-#include <Rpc.h>
-#else
-#include <uuid/uuid.h>
-#endif
-}
+#include <chrono>
+#include <random>
 
-#include <algorithm>
+std::string virgil::cli::guid () {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
 
-std::string virgil::cli::guid() {
-#ifdef WIN32
-    UUID uuid;
-    UuidCreate (&uuid);
-    unsigned char * str;
-    UuidToStringA (&uuid, &str);
-    std::string s((char*) str);
-    RpcStringFreeA (&str);
-#else
-    uuid_t uuid;
-    uuid_generate_random (uuid);
-    char buf[37] = {0x0};
-    uuid_unparse (uuid, buf);
-    std::string s(buf);
-#endif
-    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-    return s;
+    uint32_t time_low = ((generator() << 16) & 0xffff0000) | (generator() & 0xffff);
+    uint16_t time_mid = generator() & 0xffff;
+    uint16_t time_high_and_version = (generator() & 0x0fff) | 0x4000;
+    uint16_t clock_seq = (generator() & 0x3fff) | 0x8000;
+    uint8_t node [6];
+    for (size_t i = 0; i < 6; ++i) {
+        node[i] = generator() & 0xff;
+    }
+
+    char buffer[37] = {0x0};
+    sprintf(buffer, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+        time_low, time_mid, time_high_and_version, clock_seq >> 8, clock_seq & 0xff,
+        node[0], node[1], node[2], node[3], node[4], node[5]);
+
+    return std::string(buffer);
 }
