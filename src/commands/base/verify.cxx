fd52570f23df3b8b762bd98953f84d9256a88270
VladEvka <vladislav.evka@gmail.com>
2016-02-26 10:34:22 +0200

    Add base: verify

--- src/commands/base/verify.cxx
+++ src/commands/base/verify.cxx
@@ -46,13 +46,15 @@
 #include <virgil/crypto/VirgilStreamSigner.h>
 #include <virgil/crypto/stream/VirgilStreamDataSource.h>
 
+#include <virgil/sdk/model/Card.h>
+
 #include <cli/version.h>
 #include <cli/pair.h>
 #include <cli/util.h>
 
-using virgil::crypto::VirgilByteArray;
-using virgil::crypto::VirgilStreamSigner;
-using virgil::crypto::stream::VirgilStreamDataSource;
+namespace vcrypto = virgil::crypto;
+namespace vsdk = virgil::sdk;
+namespace vcli = virgil::cli;
 
 #ifdef SPLIT_CLI
 #define MAIN main
@@ -60,15 +62,19 @@ using virgil::crypto::stream::VirgilStreamDataSource;
 #define MAIN verify_main
 #endif
 
+
+static void checkFormatRecipientArg(const std::pair<std::string, std::string>& pairRecipientArg);
+
+
 int MAIN(int argc, char **argv) {
     try {
         std::string description = "Verify data and signature with given user's identifier"
-                " or with it Virgil Public Key.\n";
+                " or with it Virgil Card.\n";
 
         std::vector <std::string> examples;
         examples.push_back(
-                "virgil sign -i plain.txt -o plain.txt.sign -k private.key\n"
-                );
+                "virgil sign -i plain.txt -o plain.txt.sign -k private.key -r email:bob@gmail.com\n"
+        );
 
         std::string descriptionMessage = virgil::cli::getDescriptionMessage(description, examples);
 
@@ -85,55 +91,67 @@ int MAIN(int argc, char **argv) {
         TCLAP::ValueArg<std::string> signArg("s", "sign", "Digest sign.",
                 true, "", "file");
 
-        TCLAP::ValueArg<std::string> publicKeyIdArg("e", "public-key-id",
-                "Sign owner, defined in format:\n"
-                "[id|vkey|email]:<value>\n"
+        TCLAP::ValueArg<std::string> recipientArg("r", "recipient",
+                "Recipient defined in format:\n"
+                "[id|vcard|email]:<value>\n"
                 "where:\n"
-                "\t* if id, then <value> - signers's Virgil Public Key identifier;\n"
-                "\t* if vkey, then <value> - signers's Virgil Public Key file stored locally;\n"
-                "\t* if email, then <value> - signers's email;\n",
+                "if `id`, then <value> - UUID associated with Virgil Card identifier;\n"
+                "if `vcard`, then <value> - user's Virgil Card file stored locally;\n"
+                "if `email`, then <value> - user email associated with Public Key.",
                 true, "", "arg");
 
-        cmd.add(publicKeyIdArg);
+        cmd.add(recipientArg);
         cmd.add(signArg);
         cmd.add(outArg);
         cmd.add(inArg);
         cmd.parse(argc, argv);
 
-        // // Prepare input
-        // std::istream* inStream;
-        // std::ifstream inFile;
-        // if (inArg.getValue().empty() || inArg.getValue() == "-") {
-        //     inStream = &std::cin;
-        // } else {
-        //     inFile.open(inArg.getValue(), std::ios::in | std::ios::binary);
-        //     if (!inFile) {
-        //         throw std::invalid_argument("can not read file: " + inArg.getValue());
-        //     }
-        //     inStream = &inFile;
-        // }
+        auto recipientFormat = vcli::parsePair(recipientArg.getValue());
+        checkFormatRecipientArg(recipientFormat);
 
-        // // Verify data
-        // VirgilStreamDataSource dataSource(*inStream);
+        // Prepare input
+        std::istream* inStream;
+        std::ifstream inFile;
+        if (inArg.getValue().empty() || inArg.getValue() == "-") {
+            inStream = &std::cin;
+        } else {
+            inFile.open(inArg.getValue(), std::ios::in | std::ios::binary);
+            if (!inFile) {
+                throw std::invalid_argument("can not read file: " + inArg.getValue());
+            }
+            inStream = &inFile;
+        }
 
-        // // Read sign
-        // std::ifstream signFile(signArg.getValue(), std::ios::in | std::ios::binary);
-        // if (!signFile) {
-        //     throw std::invalid_argument("can not read file: " + signArg.getValue());
-        // }
+        // Verify data
+        vcrypto::stream::VirgilStreamDataSource dataSource(*inStream);
 
-        // VirgilByteArray sign((std::istreambuf_iterator<char>(signFile)), std::istreambuf_iterator<char>());
+        // Read sign
+        std::ifstream signFile(signArg.getValue(), std::ios::in | std::ios::binary);
+        if (!signFile) {
+            throw std::invalid_argument("can not read file: " + signArg.getValue());
+        }
 
-        // // Create signer
-        // VirgilStreamSigner signer;
-        // bool verified = signer.verify(dataSource, sign, publicKey.key());
-        // if (verified) {
-        //     virgil::cli::writeBytes(outArg.getValue(), "success");
-        //     return EXIT_SUCCESS;
-        // } else {
-        //     virgil::cli::writeBytes(outArg.getValue(), "failure");
-        //     return EXIT_FAILURE;
-        // }
+        vcrypto::VirgilByteArray sign((std::istreambuf_iterator<char>(signFile)), std::istreambuf_iterator<char>());
+
+        // Create signer
+        vcrypto::VirgilStreamSigner signer;
+
+        std::string type = recipientFormat.first;
+        std::string value = recipientFormat.second;
+        std::vector<vsdk::model::Card> recipientCards = vcli::getRecipientCards(type, value);
+
+        for(const auto& recipientCard : recipientCards) {
+            bool verified = signer.verify(dataSource, sign, recipientCard.getPublicKey().getKey());
+            std::string recipientCardId = recipientCard.getId();
+            if (verified) {
+                vcli::writeBytes(outArg.getValue(), recipientCardId + " success");
+                return EXIT_SUCCESS;
+            } else {
+                vcli::writeBytes(outArg.getValue(), recipientCardId + " failure");
+                return EXIT_FAILURE;
+            }
+        }
+
     } catch (TCLAP::ArgException& exception) {
         std::cerr << "Error: " << exception.error() << " for arg " << exception.argId() << std::endl;
         return EXIT_FAILURE;
@@ -144,3 +162,13 @@ int MAIN(int argc, char **argv) {
 
     return EXIT_SUCCESS;
 }
+
+void checkFormatRecipientArg(const std::pair<std::string, std::string>& pairRecipientArg) {
+    const std::string type = pairRecipientArg.first;
+    if (type != "id" && type != "vcard" && type != "email") {
+        throw std::invalid_argument(
+                    "invalid type format: " + type + ". Expected format: '<key>:<value>'. "
+                                                     "Where <key> = [id|vcard|email]");
+    }
+}
+
