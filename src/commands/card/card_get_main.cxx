e956f131c3cc6c2f00facc554e19ae49742ee216
VladEvka <vladislav.evka@gmail.com>
2016-03-14 17:19:27 +0200

    'card-get' - saves Cards found in separate files

--- src/commands/card/card_get_main.cxx
+++ src/commands/card/card_get_main.cxx
@@ -64,17 +64,18 @@ int MAIN(int argc, char** argv) {
 
         std::vector<std::string> examples;
         examples.push_back("Receive a Card by card-id:\n"
-                           "Virgil card-get -a <card-id>\n");
+                           "Virgil card-get -a <card-id> -o cards/\n");
 
         examples.push_back("Receive the Cards connected by public-key-id:\n"
-                           "virgil card-get -a <card-id> -e <public-key-id> -k <private_key>\n");
+                           "virgil card-get -a <card-id> -e <public-key-id> -k <private_key> -o cards/\n");
 
         std::string descriptionMessage = virgil::cli::getDescriptionMessage(description, examples);
 
         // Parse arguments.
         TCLAP::CmdLine cmd(descriptionMessage, ' ', virgil::cli_version());
 
-        TCLAP::ValueArg<std::string> outArg("o", "out", "Virgil Card. If omitted, stdout is used.", false, "", "file");
+        TCLAP::ValueArg<std::string> outArg("o", "out", "Folder in which will be saved a Virgil Cards", false, "",
+                "arg");
 
         TCLAP::ValueArg<std::string> cardIdArg("a", "card-id", "Virgil Card identifier", true, "", "arg");
 
@@ -95,13 +96,34 @@ int MAIN(int argc, char** argv) {
             vcrypto::VirgilByteArray privateKeyPass = vcli::setPrivateKeyPass(privateKey);
             vsdk::Credentials credentials(privateKey, privateKeyPass);
 
-            std::vector<vsdk::models::CardModel> cards =
+            std::vector<vsdk::models::CardModel> foundCards =
                 servicesHub.card().get(publicKeyIdArg.getValue(), cardIdArg.getValue(), credentials);
-            std::string cardsStr = vsdk::io::cardsToJson(cards, 4);
-            vcli::writeBytes(outArg.getValue(), cardsStr);
 
-            std::cout << "Cards connected by public-key-id:" << publicKeyIdArg.getValue() << " have been received"
-                      << std::endl;
+            if (foundCards.empty()) {
+                std::cout << "Cards by card-id: " << cardIdArg.getValue()
+                          << " and public-key-id: " << publicKeyIdArg.getValue() << " haven't been found." << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            std::string pathTofolder = outArg.getValue();
+            if (pathTofolder.empty()) {
+                for(auto&& foundCard : foundCards) {
+                    std::string foundCardStr = vsdk::io::Marshaller<vsdk::models::CardModel>::toJson<4>(foundCard);
+                    vcli::writeBytes(pathTofolder, foundCardStr);
+                }
+            } else {
+                for(auto&& foundCard : foundCards) {
+                    std::string identity = foundCard.getCardIdentity().getValue();
+                    std::string cardId = foundCard.getId();
+
+                    std::string fileName = identity + "-id-" + foundCard.getId() + ".vcard";
+                    std::string foundCardStr = vsdk::io::Marshaller<vsdk::models::CardModel>::toJson<4>(foundCard);
+                    vcli::writeBytes(pathTofolder + "/" + fileName, foundCardStr);
+                }
+            }
+
+            std::cout << "Cards by card-id: " << cardIdArg.getValue()
+                      << " and public-key-id: " << publicKeyIdArg.getValue() << " have been found." << std::endl;
         } else {
             vsdk::models::CardModel card = servicesHub.card().get(cardIdArg.getValue());
             std::string cardStr = vsdk::io::Marshaller<vsdk::models::CardModel>::toJson<4>(card);
