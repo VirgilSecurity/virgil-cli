ec875a02d4de93ef64e8ef9c3f09c9a8809d41f0
Sergey Seroshtan <sergey.seroshtan@gmail.com>
2016-11-18 14:23:40 +0200

    [CLI-124] Add external dependency: DocOpt

---
+++ ext/docopt/docopt.cmake
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2016 Virgil Security Inc.
+#
+# Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     (1) Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#     (2) Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#
+#     (3) Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived from
+#     this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+cmake_minimum_required (VERSION @CMAKE_VERSION@ FATAL_ERROR)
+
+project ("@VIRGIL_DEPENDS_PACKAGE_NAME@-depends")
+
+include (ExternalProject)
+
+# Configure additional CMake parameters
+file (WRITE "@VIRGIL_DEPENDS_ARGS_FILE@"
+    "set (WITH_TESTS OFF CACHE INTERNAL \"\")\n"
+    "set (WITH_EXAMPLE OFF CACHE INTERNAL \"\")\n"
+    "set (USE_BOOST_REGEX OFF CACHE INTERNAL \"\")\n"
+)
+
+ExternalProject_Add (${PROJECT_NAME}
+    DOWNLOAD_DIR "@VIRGIL_DEPENDS_PACKAGE_DOWNLOAD_DIR@"
+    URL "https://github.com/docopt/docopt.cpp//archive/af03fa044ee1eff20819549b534ea86829a24a54.tar.gz"
+    URL_HASH SHA1=c86efaa319d079bf231c7fa48d4f86437684e94d
+    PREFIX "@VIRGIL_DEPENDS_PACKAGE_BUILD_DIR@"
+    CMAKE_ARGS "@VIRGIL_DEPENDS_CMAKE_ARGS@"
+)
+
+add_custom_target ("${PROJECT_NAME}-build" ALL COMMENT "Build package ${PROJECT_NAME}")
+add_dependencies ("${PROJECT_NAME}-build" ${PROJECT_NAME})
