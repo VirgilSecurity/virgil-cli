e5aa257b153477246b4a8f7018d3a0888fd7cfbe
Oleksii Holubovich <agolubovich94@gmail.com>
2019-09-03 16:17:55 +0300

    rm reduntant log

--- client/client.go
+++ client/client.go
@@ -116,7 +116,6 @@ func (vc *VirgilHttpClient) Send(method string, urlPath string, payload interfac
 	}
 
 	if len(respBody) > 0 {
-		fmt.Println("error from server : " + string(respBody))
 		httpErr := &VirgilAPIError{}
 		err = json.Unmarshal(respBody, httpErr)
 		if err == nil {
